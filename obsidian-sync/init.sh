76b288cb9c94f2e5a72c4b03ef9b739e899b15a1
handgarden <heehos3@gmail.com>
2024-02-17 13:55:48 +0000

    fix: obsidian init script

--- obsidian-sync/init.sh
+++ obsidian-sync/init.sh
@@ -15,15 +15,27 @@ if [ -f "$ENV_FILE" ]; then
                 "password")
                     password="$value"
                     ;;
+	    	"hostname")
+		    hostname="$value"
+		    ;;
             esac
         fi
     done < "$ENV_FILE"
 
-    curl -X PUT "http://${username}:${password}@127.0.0.1:5984/_users"
-    curl -X PUT "http://${username}:${password}@127.0.0.1:5984/_replicator"
-    curl -X PUT "http://${username}:${password}@127.0.0.1:5984/_global_changes"
+    until (curl -X POST "${hostname}/_cluster_setup" -H "Content-Type: application/json" -d "{\"action\":\"enable_single_node\",\"username\":\"${username}\",\"password\":\"${password}\",\"bind_address\":\"0.0.0.0\",\"port\":5984,\"singlenode\":true}" --user "${username}:${password}"); do sleep 5; done
+	until (curl -X PUT "${hostname}/_node/nonode@nohost/_config/chttpd/require_valid_user" -H "Content-Type: application/json" -d '"true"' --user "${username}:${password}"); do sleep 5; done
+	until (curl -X PUT "${hostname}/_node/nonode@nohost/_config/chttpd_auth/require_valid_user" -H "Content-Type: application/json" -d '"true"' --user "${username}:${password}"); do sleep 5; done
+	until (curl -X PUT "${hostname}/_node/nonode@nohost/_config/httpd/WWW-Authenticate" -H "Content-Type: application/json" -d '"Basic realm=\"couchdb\""' --user "${username}:${password}"); do sleep 5; done
+	until (curl -X PUT "${hostname}/_node/nonode@nohost/_config/httpd/enable_cors" -H "Content-Type: application/json" -d '"true"' --user "${username}:${password}"); do sleep 5; done
+	until (curl -X PUT "${hostname}/_node/nonode@nohost/_config/chttpd/enable_cors" -H "Content-Type: application/json" -d '"true"' --user "${username}:${password}"); do sleep 5; done
+	until (curl -X PUT "${hostname}/_node/nonode@nohost/_config/chttpd/max_http_request_size" -H "Content-Type: application/json" -d '"4294967296"' --user "${username}:${password}"); do sleep 5; done
+	until (curl -X PUT "${hostname}/_node/nonode@nohost/_config/couchdb/max_document_size" -H "Content-Type: application/json" -d '"50000000"' --user "${username}:${password}"); do sleep 5; done
+	until (curl -X PUT "${hostname}/_node/nonode@nohost/_config/cors/credentials" -H "Content-Type: application/json" -d '"true"' --user "${username}:${password}"); do sleep 5; done
+	until (curl -X PUT "${hostname}/_node/nonode@nohost/_config/cors/origins" -H "Content-Type: application/json" -d '"app://obsidian.md,capacitor://localhost,http://localhost"' --user "${username}:${password}"); do sleep 5; done
+
+	echo "<-- Configuring CouchDB by REST APIs Done!"
 else
-    echo "$ENV_FILE 파일을 찾을 수 없습니다."
+    echo "$ENV_FILE MISSING."
     exit 1
 fi
 
